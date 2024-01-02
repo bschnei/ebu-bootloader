70b42b46a02ed2a2dcb5b133f84f027cda0d6e5b
Benjamin Schneider <ben@bens.haus>
2024-01-01 20:30:29 -0800

    flatten build script stucture

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ ROOT_DIR=$(pwd)
 # cross compile settings for make
 export ARCH=arm64
 export CROSS_COMPILE=${ROOT_DIR}/toolchain/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+# Marvell's default assumption lines up with Ubuntu's package name for the
+# 32-bit compiler. Set explicitly or modify as needed.
 #export CROSS_CM3=/usr/bin/arm-linux-gnueabi-
 
 # location of source code
@@ -12,19 +15,6 @@ export a3700_utils=${ROOT_DIR}/A3700-utils-marvell
 export atf=${ROOT_DIR}/trusted-firmware-a
 export uboot=${ROOT_DIR}/u-boot-marvell
 
-
-function query_commitid {
-    local path=$1
-
-    # query latest commit
-    if [ -d "$path/.git" ]; then
-        commitid=$(git -C "$path" log --no-merges --pretty=format:"%h%n" -1)
-    else
-        commitid="0000000"
-    fi
-
-    echo "$commitid"
-}
 
 function build_uboot {
 
@@ -39,15 +29,23 @@ function build_uboot {
     return 0
 }
 
-# build ARM Trusted Firmware
-function build_atf {
-    local cpu_speed=$1
-    
+function build_bootloader {
+
+    # U-Boot needs to build successfully first since it gets
+    # integrated into the final image file
+    build_uboot
+
+    # see README
+    local cpu_speed=1000
+
     # DDR clock speed is a function of cpu_speed
     local ddr_speed=800
     if [ "$cpu_speed" == 1200 ]; then
         ddr_speed=750
     fi
+
+    # path to A3700 git repository (must be a git repo)
+    local a3700_utils=${ROOT_DIR}/A3700-utils-marvell
 
     # clean a3700-utils image to prevent using old ddr image
     make -C "$a3700_utils" clean DDR_TOPOLOGY=5
@@ -69,21 +67,6 @@ function build_atf {
     # NOTE: in older ATF versions, a build target that is only "mrvl_flash" or only "fip" will fail to boot, so use "all fip"
     # This does not seem to be an issue in newer versions of ATF--"mrvl flash" seems like it should work, but testing is needed.
 
-    return 0
-}
-
-function build_bootloader {
-
-    # U-Boot needs to build successfully first since it gets
-    # integrated into a single Firmware Image Package (FIP) file as part of
-    # building the rest of the ARM Trusted Firmware
-    build_uboot
-
-    # see README
-    local cpu_speed=1000
-
-    build_atf $cpu_speed
-
     if [ ! -f "$atf/build/a3700/release/flash-image.bin" ]; then
         echo "Failed to build FIP!"
         return 0
@@ -91,25 +74,12 @@ function build_bootloader {
 
     # package the output
     local build_path
-    build_path=${ROOT_DIR}/build/$(date +"%Y%m%d-%H%M")
+    build_path=${ROOT_DIR}/build/
     mkdir -p "$build_path"
 
-    # get commit ids
-    local git_wtp
-    git_wtp=$(query_commitid "$a3700_utils")
-
-    local git_atf
-    git_atf=$(query_commitid "$atf")
-
-    local git_uboot
-    git_uboot=$(query_commitid "$uboot")
-
-    # record build settings
-    local infomsg="CPU: ${cpu_speed}\nATF: g${git_atf}\nU-Boot: g${git_uboot}\nA3700 Utils: g${git_wtp}"
-    echo -e "$infomsg" > "$build_path"/buildinfo.txt
-
     # copy image to output folder
-    cp "$atf/build/a3700/release/flash-image.bin" "$build_path/flash.bin"
+    cp "$atf/build/a3700/release/flash-image.bin" "$build_path/$(date +"%Y%m%d-%H%M").bin"
+    cp "$atf/build/a3700/release/flash-image.bin" "$build_path/latest.bin"
     sync
 
 }

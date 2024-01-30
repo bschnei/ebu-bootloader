7de76b609330b9d18027d85cd4280f7550a968d8
Benjamin Schneider <ben@bens.haus>
2024-01-29 17:43:27 -0800

    log filename instead of ISO time in log file when archiving

--- archive.sh
+++ archive.sh
@@ -3,6 +3,7 @@
 BASE_PATH=$(cd "$(dirname "$BASH_SOURCE")"; cd -P "$(dirname "$(readlink "$BASH_SOURCE" || echo .)")"; pwd)
 ARCHIVE_PATH=${BASE_PATH}/build/
 TFA_BUILD_PATH=${BASE_PATH}/trusted-firmware-a/build/a3700/release/flash-image.bin
+BUILD_TIME=$(date +"%Y%m%d-%H%M")
 
 # build
 make clean
@@ -10,7 +11,7 @@ if ! (make CLOCKSPRESET=CPU_1000_DDR_800); then exit $?; fi
 
 # archive
 mkdir -p ${ARCHIVE_PATH}
-cp ${TFA_BUILD_PATH} "$ARCHIVE_PATH/$(date +"%Y%m%d-%H%M").bin"
+cp ${TFA_BUILD_PATH} "$ARCHIVE_PATH/$BUILD_TIME.bin"
 cp ${TFA_BUILD_PATH} "$ARCHIVE_PATH/latest.bin"
 
 # build
@@ -21,5 +22,5 @@ if ! (make CLOCKSPRESET=CPU_1200_DDR_750); then exit $?; fi
 cp ${TFA_BUILD_PATH} "$ARCHIVE_PATH/test.bin"
 
 # log
-echo "$(date --iso-8601=minutes)  $(git log -n 1 --pretty=oneline --abbrev-commit)" >> ${ARCHIVE_PATH}/log.txt
+echo "$BUILD_TIME  $(git log -n 1 --pretty=oneline --abbrev-commit)" >> ${ARCHIVE_PATH}/log.txt
 sync
